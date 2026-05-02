511b3d8cecfaca2d8273013ff1f6953e314b1176
0xarchit <0xarchit@gmail.com>
2026-05-02 12:02:26 +0530

    feat: implement Stunning UI overhaul and custom media selection support

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/pauseCat.ico");
         res.set_manifest_file("assets/app.manifest");
         res.compile().unwrap();
     }
