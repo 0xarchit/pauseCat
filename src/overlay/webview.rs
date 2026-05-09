f20a8889a4b9013a22ba7eeb5e1c051a10d9d3fc
0xarchit <0xarchit@gmail.com>
2026-05-09 19:59:15 +0530

    feat: high-fidelity 3D text engine with granular glow, rotation, and persistence

--- src/overlay/webview.rs
+++ src/overlay/webview.rs
@@ -43,13 +43,13 @@ where F: FnOnce(&str) {
         };
         let messages_json = serde_json::to_string(&settings.break_messages).unwrap_or_else(|_| "[]".to_string());
         let init_msg = format!(
-            "{{\"action\":\"init\", \"duration\": {}, \"mode\": \"{}\", \"mediaPath\": \"{}\", \"isDark\": {}, \"bubbleOpacity\": {}, \"bubbleSize\": {}, \"bubblePosX\": {}, \"bubblePosY\": {}, \"animationStyle\": \"{}\", \"breakMessages\": {}, \"randomizeMessages\": {}, \"showWorkStatus\": {}, \"workDurationSecs\": {}, \"breakStyle\": \"{}\", \"customText\": \"{}\", \"videoVolume\": {}, \"textAnimation\": \"{}\", \"textRotationX\": {}, \"textRotationY\": {}, \"textColor\": \"{}\", \"textOpacity\": {}, \"textGlow\": {}, \"textDepth\": {}, \"adaptiveTextColor\": {}}}",
+            "{{\"action\":\"init\", \"duration\": {}, \"mode\": \"{}\", \"mediaPath\": \"{}\", \"isDark\": {}, \"bubbleOpacity\": {}, \"bubbleSize\": {}, \"bubblePosX\": {}, \"bubblePosY\": {}, \"animationStyle\": \"{}\", \"breakMessages\": {}, \"randomizeMessages\": {}, \"showWorkStatus\": {}, \"workDurationSecs\": {}, \"breakStyle\": \"{}\", \"customText\": \"{}\", \"videoVolume\": {}, \"textAnimation\": \"{}\", \"textRotationX\": {}, \"textRotationY\": {}, \"textRotationZ\": {}, \"textColor\": \"{}\", \"textOpacity\": {}, \"textGlow\": {}, \"textGlowEnabled\": {}, \"textGlowColor\": \"{}\", \"textDepth\": {}, \"adaptiveTextColor\": {}}}",
             settings.break_duration_secs, mode_str, final_media_path, crate::system::is_dark_mode(),
             settings.bubble_opacity, settings.bubble_size, settings.bubble_pos_x, settings.bubble_pos_y,
             settings.animation_style, messages_json, settings.randomize_messages, settings.show_work_duration_status, settings.work_duration_secs,
             settings.break_style, settings.custom_text.replace("\"", "\\\""), settings.video_volume,
-            settings.text_animation, settings.text_rotation_x, settings.text_rotation_y, settings.text_color, settings.text_opacity,
-            settings.text_glow, settings.text_depth, settings.adaptive_text_color
+            settings.text_animation, settings.text_rotation_x, settings.text_rotation_y, settings.text_rotation_z, settings.text_color, settings.text_opacity,
+            settings.text_glow, settings.text_glow_enabled, settings.text_glow_color, settings.text_depth, settings.adaptive_text_color
         );
         post_message(&init_msg);
     }
